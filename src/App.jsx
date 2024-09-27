f2a0202c6cc812ed34f2e90063579f8b64040126
DR Dev <dromeroldev@gmail.com>
2024-09-27 18:18:12 -0500

    Created fetch from coffee list api

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ApiCoffeeCarrouselBasic } from './components/ApiCoffeeCarrouselBasic'
+
+function App() {
+
+  return (
+    <div>
+      <ApiCoffeeCarrouselBasic />
+    </div>
+  )
+}
+
+export default App
